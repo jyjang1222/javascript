c2b1b6a961e1dc5f144ac366944aec2eeddbc37b
JJY <jyjang1222@naver.com>
2022-11-28 17:51:00 +0900

    readme 및 섹션2 소스  업데이트

--- section02/basics-01-starting-project/assets/scripts/app.js
+++ section02/basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // 사용자 입력을 받아옴
 function getUserNumberInput() {
@@ -10,6 +11,17 @@ function getUserNumberInput() {
 function createAndWriteOutput(operater, resultBeforeCalc, calcNumber) {
   const calcDescription = `${resultBeforeCalc} ${operater} ${calcNumber}`;
   outputResult(currentResult, calcDescription); //vender.js 에서 옴
+}
+
+// 로그생성
+function writeToLog(operationIdentifier, prevResult, operationNumber, newResult) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult
+  };
+  logEntries.push(logEntry);
 }
 
 function add() {
@@ -24,6 +36,13 @@ function subtract() {
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'SUBTRACT',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
 }
 
 function multiply() {
