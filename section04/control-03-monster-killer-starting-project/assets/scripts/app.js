1fec512dde462caffa4363141b895ecc2d67cbb1
JJY <jyjang1222@naver.com>
2022-12-01 21:56:37 +0900

    힐 관련문제 코드 수정

--- section04/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ section04/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -12,22 +12,25 @@ adjustHealthBars(chosenMaxLife);
 // 공격진행 및 결과체크
 function actAndChkResult(actType) {
   let attackType = PLAYER_ATTACK_VALUE;
-  if (actType == 'STRONG_ATTACK') {
+  if (actType === 'STRONG_ATTACK') {
     attackType = PLAYER_STRONG_ATTACK_VALUE;
   }
-  // 플레이어의 공격
-  const monsterDamage = dealMonsterDamage(attackType);
-  currentMonsterHealth -= monsterDamage; //현재 남은 몬스터체력에 데미지 적용
+  if (actType === 'ATTACK' || actType === 'STRONG_ATTACK') {
+    // 플레이어의 공격
+    const monsterDamage = dealMonsterDamage(attackType);
+    currentMonsterHealth -= monsterDamage; //현재 남은 몬스터체력에 데미지 적용
+  }
+  
   //힐
-  if (actType === 'HEAL') {
-    let healValue = HEAL_VALUE;
+  if (actType == 'HEAL') {
+    let healValue2 = HEAL_VALUE;
     if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+      healValue2 = chosenMaxLife - currentPlayerHealth;
       currentPlayerHealth = chosenMaxLife;
-      healValue = chosenMaxLife - currentPlayerHealth;
     } else {
       currentPlayerHealth += HEAL_VALUE;
     }
-    increasePlayerHealth(healValue);
+    increasePlayerHealth(healValue2);
   }
   // 몬스터의 반격
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
