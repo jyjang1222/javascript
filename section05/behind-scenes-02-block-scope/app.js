cf250aab1f14d7ff199fa8899722520af18837a4
JJY <jyjang1222@naver.com>
2022-12-12 11:21:23 +0900

    자스 학습내용 중간 업데이트

---
+++ section05/behind-scenes-02-block-scope/app.js
@@ -0,0 +1,21 @@
+// var name = 'jun';
+
+// if (name === 'jun') {
+//   // var hobbies = ['Sports', 'Cooking']; 전역변수
+//   let hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
+// }
+
+// function greet() {
+//   var age = 30;
+//   var name = 'max';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+
+console.log(userName);
+
+let userName = 'jun';
