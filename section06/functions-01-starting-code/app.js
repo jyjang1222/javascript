8be15e45488b7ad504d6589ec7b3873987fef00e
JJY <jyjang1222@naver.com>
2022-12-13 22:53:49 +0900

    update

--- section06/functions-01-starting-code/app.js
+++ section06/functions-01-starting-code/app.js
@@ -1,11 +1,25 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const SCISSORS = '가위';
+const ROCK = '바위';
+const PAPER = '보';
+const DEFAULT_USER_CHOICE = ROCK;
+
 const getPlayerChoice = function () {
-  const selection = prompt('가위, 바위, 보 중 하나를 입력하세요.', '');
-  if (selection !== '가위' && selection !== '바위' && selection !== '보') {
+  const selection = prompt(`${SCISSORS}, ${ROCK}, ${PAPER} 중 하나를 입력하세요.`, '');
+  if (
+    selection !== SCISSORS &&
+    selection !== ROCK &&
+    selection !== PAPER
+    ) {
+    alert(`잘못입력했습니다. ${DEFAULT_USER_CHOICE}가 선택됩니다.`);
+    return DEFAULT_USER_CHOICE;
   }
+  return selection;
 };
 
 startGameBtn.addEventListener('click', function () {
   console.log('Game is starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
