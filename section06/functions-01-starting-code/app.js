51540fe364e32e4945d247e7fd928924072b8633
JJY <jyjang1222@naver.com>
2022-12-13 14:00:53 +0900

    가위바위보 중간 업데이트

--- section06/functions-01-starting-code/app.js
+++ section06/functions-01-starting-code/app.js
@@ -1,19 +1,11 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function startGame() {
-//   console.log('Game is starting...');
-// };
-
-// const person = {
-//   name: 'Max',
-//   greet: function greet() {
-//     console.log('hello there');
-//   }
-// };
-// person.greet();
-
-// console.dir(startGame);
+const getPlayerChoice = function () {
+  const selection = prompt('가위, 바위, 보 중 하나를 입력하세요.', '');
+  if (selection !== '가위' && selection !== '바위' && selection !== '보') {
+  }
+};
 
 startGameBtn.addEventListener('click', function () {
-  console.log('Game is starting...', age);
+  console.log('Game is starting...');
 });
